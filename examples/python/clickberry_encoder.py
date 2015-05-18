7a6824a9c4459d39812a5d73dbff2ae055cfee37
alexey-ernest <alexey.ernest@gmail.com>
2015-05-18 11:35:27 +0300

    Python example

---
+++ examples/python/clickberry_encoder.py
@@ -0,0 +1,12 @@
+"""
+Example client for Clickberry Encoder https://github.com/clickberry/video-encoder
+"""
+import requests
+
+URL = 'http://clickberry.encoder.domain/api/videos'
+FILE = r'path/to/video.mp4'
+
+with open(FILE, 'rb') as file_data:
+    FILES = {'file': ('video', file_data, 'video/mp4')}
+    REQUEST = requests.post(URL, files=FILES)
+    print REQUEST.text
